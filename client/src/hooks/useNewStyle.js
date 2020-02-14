bb2c54b0d535947b9eaadffd7c1b7514af62b7b7
Alex Miller <alexmillerprojects@gmail.com>
2020-02-14 08:55:14 -0800

    added style switcher, working on testing

--- client/src/hooks/useNewStyle.js
+++ client/src/hooks/useNewStyle.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useNewStyle = (key, initialValue) => {
@@ -5,11 +6,13 @@ export const useNewStyle = (key, initialValue) => {
     //sets the new style state to the value return from the local storage whether it was the saved value in the storage, or the default value for new users
     const [newStyle, setNewStyle] = useLocalStorage(key, initialValue)
 
-    const changeStyle = e => {
-        setNewStyle({
-            newStyle: !newStyle
-        })
-    }
+    useEffect(() => {
+        if (newStyle === true) { 
+            return document.querySelector('html').classList.add('style1')
+        } else {
+            return document.querySelector('html').classList.remove('style1')
+        }
+    })
 
-    return [newStyle, changeStyle];
+    return [newStyle, setNewStyle];
 }
